a7c0ccef0543d137484382b936d39d1823ffaf4f
Valery <Vatazhok.Valery@gmail.com>
2021-03-28 00:48:16 +0200

    add 2 part to task 5

--- task_5.php
+++ task_5.php
@@ -5,11 +5,12 @@ $users["3"] = ["name" => "anton", "email" => "anton@gmail.com", "lang" => "fr"];
 $users["0"] = ["name" => "ira", "email" => "igor@test.com", "lang" => "ua"];
 $users["1"] = ["name" => "artem", "email" => "ira@gmail.com", "lang" => "en"];
 $users["4"] = ["name" => "artem", "email" => "katya@test.com", "lang" => "ru"];
-$users["2"] = ["name" => "artem", "email" => "artem@gmail.com", "lang" => "de"];
+$users["2"] = ["name" => "artem", "email" => "artem@gmail.com", "lang" => "ua"];
 
 
 krsort($users);
-
+$arr=[];
+$arr2=[];
 for($i=0; $i<count($users); $i++){
     $arr[]=$users[$i]["name"];
 }
@@ -23,9 +24,34 @@ foreach ($arr2 as $key => $value){
         echo "Ім'я: ".$key.', '.'кількість повторів: '.$value.';'.'</br>';
     }
 }
+$en=[];
+$ua=[];
+$ru=[];
+$fr=[];
 
 
+for($i=0; $i<count($users); $i++){
+    if($users[$i]["lang"]=="en"){
+        $en[] = $users[$i];
+    }
+    elseif($users[$i]["lang"]=="ua"){
+        $ua[] = $users[$i];
+    }
+    elseif($users[$i]["lang"]=="ru"){
+        $ru[] = $users[$i];
+    }
+    elseif($users[$i]["lang"]=="fr"){
+        $fr[] = $users[$i];
+    }
 
+}
+print_r($en);
+echo '</br>';
+print_r($ua);
+echo '</br>';
+print_r($ru);
+echo '</br>';
+print_r($fr);
 
 
 
