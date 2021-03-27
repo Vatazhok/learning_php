2a6d17c9709f789b98047bec852b7752eac2a4e7
Valery <Vatazhok.Valery@gmail.com>
2021-03-28 01:22:14 +0200

    finished with the task 5

--- task_5.php
+++ task_5.php
@@ -1,5 +1,5 @@
 <?php
-$users=[];
+$users = [];
 $users["5"] = ["name" => "ira", "email" => "ivan@test.com", "lang" => "en"];
 $users["3"] = ["name" => "anton", "email" => "anton@gmail.com", "lang" => "fr"];
 $users["0"] = ["name" => "ira", "email" => "igor@test.com", "lang" => "ua"];
@@ -8,43 +8,42 @@ $users["4"] = ["name" => "artem", "email" => "katya@test.com", "lang" => "ru"];
 $users["2"] = ["name" => "artem", "email" => "artem@gmail.com", "lang" => "ua"];
 
 
-krsort($users);
-$arr=[];
-$arr2=[];
-for($i=0; $i<count($users); $i++){
-    $arr[]=$users[$i]["name"];
+ksort($users);
+$arr = [];
+$arr2 = [];
+for ($i = 0; $i < count($users); $i++) {
+    $arr[] = $users[$i]["name"];
 }
 //print_r($arr);
 
-$arr2=array_count_values($arr);
+$arr2 = array_count_values($arr);
 //print_r($arr2);
-
-foreach ($arr2 as $key => $value){
-    if ($value >=2) {
-        echo "Ім'я: ".$key.', '.'кількість повторів: '.$value.';'.'</br>';
+echo "1:" . '</br>';
+foreach ($arr2 as $key => $value) {
+    if ($value >= 2) {
+        echo "Ім'я: " . $key . ', ' . 'кількість повторів: ' . $value . ';' . '</br>';
     }
 }
-$en=[];
-$ua=[];
-$ru=[];
-$fr=[];
+$en = [];
+$ua = [];
+$ru = [];
+$fr = [];
 
 
-for($i=0; $i<count($users); $i++){
-    if($users[$i]["lang"]=="en"){
+for ($i = 0; $i < count($users); $i++) {
+    if ($users[$i]["lang"] == "en") {
         $en[] = $users[$i];
-    }
-    elseif($users[$i]["lang"]=="ua"){
+    } elseif ($users[$i]["lang"] == "ua") {
         $ua[] = $users[$i];
-    }
-    elseif($users[$i]["lang"]=="ru"){
+    } elseif ($users[$i]["lang"] == "ru") {
         $ru[] = $users[$i];
-    }
-    elseif($users[$i]["lang"]=="fr"){
+    } elseif ($users[$i]["lang"] == "fr") {
         $fr[] = $users[$i];
     }
 
 }
+echo "2:" . '</br>';
+echo '<pre>';
 print_r($en);
 echo '</br>';
 print_r($ua);
@@ -52,7 +51,13 @@ echo '</br>';
 print_r($ru);
 echo '</br>';
 print_r($fr);
+echo '</pre>';
 
-
-
-
+$users_new = [];
+for ($i = count($users) - 1; $i >= 0; $i--) {
+    $users_new[] = $users[$i];
+}
+echo "3:" . '</br>';
+echo '<pre>';
+print_r($users_new);
+echo '</pre>';
